15a6f79d24497d2ba541d571e64476a494fd98ca
Arnoldelite <arnoldelite@yahoo.co.uk>
2017-07-06 14:14:40 -0400

    correct media store reference

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -295,7 +295,7 @@ public class Capture extends CordovaPlugin {
             PermissionHelper.requestPermission(this, req.requestCode, Manifest.permission.CAMERA);
         } else {
             Intent intent = new Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
-            intent.putExtra("android.provider.MediaStore.EXTRA_OUTPUT", Uri.fromFile(new File("file:///storage/emulated/0")));
+            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File("file:///storage/emulated/0")));
             if(Build.VERSION.SDK_INT > 7){
                 intent.putExtra("android.intent.extra.durationLimit", req.duration);
                 intent.putExtra("android.intent.extra.videoQuality", req.quality);
