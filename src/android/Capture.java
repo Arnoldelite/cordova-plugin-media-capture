88556aeceaaaa6b7adc2a40f80ee535c67cb77a1
Arnoldelite <arnoldelite@yahoo.co.uk>
2017-07-12 11:12:53 -0400

    force media scanner service to re-run

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -295,7 +295,9 @@ public class Capture extends CordovaPlugin {
             PermissionHelper.requestPermission(this, req.requestCode, Manifest.permission.CAMERA);
         } else {
             Intent intent = new Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
-            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(getTempDirectoryPath(),"Capture.mp4")));
+            //intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(getTempDirectoryPath(),"Capture.mp4")));
+            //Force android mediaScanner to run again
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
             if(Build.VERSION.SDK_INT > 7){
                 intent.putExtra("android.intent.extra.durationLimit", req.duration);
                 intent.putExtra("android.intent.extra.videoQuality", req.quality);
