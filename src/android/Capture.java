7f6c893d5ea402b69b8859b637e9121e9d4bf48e
Arnoldelite <arnoldelite@yahoo.co.uk>
2017-07-18 13:56:40 -0400

    refactor

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -295,7 +295,7 @@ public class Capture extends CordovaPlugin {
      * Sets up an intent to capture video.  Result handled by onActivityResult()
      */
     private void captureVideo(Request req) {
-        if(cameraPermissionInManifest && !PermissionHelper.hasPermission(this, Manifest.permission.CAMERA)) {
+        if (cameraPermissionInManifest && !PermissionHelper.hasPermission(this, Manifest.permission.CAMERA)) {
             PermissionHelper.requestPermission(this, req.requestCode, Manifest.permission.CAMERA);
         } else {
 
@@ -304,27 +304,27 @@ public class Capture extends CordovaPlugin {
             //intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, context.getFilesDir().getPath() + "/user/0/com.aetonix.mobileappprod/cache");
 
 //            String _path = Environment.getExternalCacheDir() + "/Camera/Video.mp4";
-//            File file = new File( _path );
-//            Uri outputFileUri = Uri.fromFile(file);
-////
-//            Intent intent = new Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
-//            //intent.putExtra( android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(getPicturesPath())) );
+            File file = new File( cordova.getActivity().getExternalCacheDir() + "/Camera/Video.mp4" );
+            Uri outputFileUri = Uri.fromFile(file);
 //
-            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.parse(new File ("/storage/emulated/0/Android/data/com.aetonix.mobileappprod/cache/Video.mp4")));
+            Intent intent = new Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
+            intent.putExtra( android.provider.MediaStore.EXTRA_OUTPUT, outputFileUri );
+//
+            //intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.parse(new File("/storage/emulated/0/Android/data/com.aetonix.mobileappprod/cache/Video.mp4")));
             Log.i("TAG", "MediaStore.EXTRA_OUTPUT variable" + android.provider.MediaStore.EXTRA_OUTPUT);
             //intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, getVideoUri());
             //Force android mediaScanner to run again
             //sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
             //sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, getVideoUri()));
             //refreshGallery();
-            if(Build.VERSION.SDK_INT > 7){
+            if (Build.VERSION.SDK_INT > 7) {
                 intent.putExtra("android.intent.extra.durationLimit", req.duration);
                 intent.putExtra("android.intent.extra.videoQuality", req.quality);
             }
             this.cordova.startActivityForResult((CordovaPlugin) this, intent, req.requestCode);
         }
 
-
+    }
 //    private void refreshGallery() {
 //        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
 //        //mediaScanIntent.setData(contentUri);
